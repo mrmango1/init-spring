fc8ea85c97346b46f0b6c048a2372e2dd148b95e
Anderson Grefa <mrmango1@DESKTOP-CDF3E3I>
2022-08-24 15:04:33 -0500

    Create springboot project

---
+++ src/main/java/com/cybertools/initspring/InitSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cybertools.initspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybertools/initspring/InitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybertools.initspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
